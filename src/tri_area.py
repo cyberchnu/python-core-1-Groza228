bf9e7c01b2d94f65e81f46788d2987ea168a0bdd
Groza <veprukandriy@ukr.net>
2024-02-15 12:16:18 +0200

    Task "addiption" has done

--- src/tri_area.py
+++ src/tri_area.py
@@ -1,3 +1,8 @@
 def tri_area(base, height):
-  # Type your code
-  return 
+    area = (base * height) / 2
+    return area
+
+
+base = int(input("Enter base: "))
+height = int(input("Enter height:"))
+print(tri_area(base, height))
