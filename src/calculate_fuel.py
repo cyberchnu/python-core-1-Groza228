4ed76c42e0e5af807d6871c92785cfa7d7ba77be
Groza <veprukandriy@ukr.net>
2024-02-15 12:19:07 +0200

    Task "Calculate_fuel" has done

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,3 +1,10 @@
 def calculate_fuel(distance):
-  # Type your code
-  return
+    fuel_needed = distance * 10
+    if fuel_needed < 0:
+        print(100)
+    else:
+        return fuel_needed
+
+
+distance = float(input("Enter distance in kilometers: "))
+print(calculate_fuel(distance))
