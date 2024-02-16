d7a7242cd683d9823ac0eee6137239f4c5d988d4
Groza <veprukandriy@ukr.net>
2024-02-16 12:49:56 +0200

    Task "int_within_bounds" has done

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,7 +1,7 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-    if number % x != 0:
+    if type(number)!=int:
         return False
-    elif number > lower_bound and number < upper_bound:
+    elif number >= lower_bound and number < upper_bound:
         return True
     else:
         return False
