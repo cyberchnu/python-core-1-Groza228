5ffd9667593ab4b2fb7b3c7d69e5499256201fab
Groza <veprukandriy@ukr.net>
2024-02-16 10:15:39 +0200

    Task "tri_area" has done

--- src/tri_area.py
+++ src/tri_area.py
@@ -1,8 +1,3 @@
 def tri_area(base, height):
     area = (base * height) / 2
     return area
-
-
-base = int(input("Enter base: "))
-height = int(input("Enter height:"))
-print(tri_area(base, height))
