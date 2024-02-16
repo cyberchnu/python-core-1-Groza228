3fdcaa3235aeb1ca299ed588f52c36bbff74063b
Groza <veprukandriy@ukr.net>
2024-02-16 10:22:02 +0200

    Task "both" has done

--- src/both.py
+++ src/both.py
@@ -1,12 +1,5 @@
 def both(number1, number2):
-    if (number1 <= 0 and number2 <= 0) or (number1 >= 0 and number2 >=0):
-        print(True)
+    if number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0 or number1 > 0 and number2 > 0:
+        return True
     else:
-        print(False)
-    return
-
-
-number1 = int(input("Enter number1: "))
-number2 = int(input("Enter number2: "))
-print(both(number1, number2))
-
+        return False
