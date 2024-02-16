18831186a44efa21b90a0e8a0940b4abe069305b
Groza <veprukandriy@ukr.net>
2024-02-16 12:55:45 +0200

    Task "calculate_fuel" has done

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -2,7 +2,7 @@ def calculate_fuel(distance):
     fuel = distance * 10
     if distance <= 0:
         return False
-    elif fuel < 0:
+    elif fuel < 100:
         return 100
     else:
         return fuel
